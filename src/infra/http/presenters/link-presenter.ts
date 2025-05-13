b3c4110fde10a707f30d2b61c967f031b4e190e2
MatthewAraujo <matthewaraujo20@gmail.com>
2025-05-12 23:24:42 -0300

    feat: add traefik proxy working

--- src/infra/http/presenters/link-presenter.ts
+++ src/infra/http/presenters/link-presenter.ts
@@ -3,7 +3,7 @@ import { Link } from "@/domain/links/enterprise/entities/link";
 export class LinkPresenter {
   static toHTTP(link: Link) {
     const host = process.env.BASE_URL ?? "http://localhost";
-    const port = process.env.PORT ?? 3000;
+    const port = process.env.REVESE_PROXY_PORT ?? 3000;
 
     const baseUrl = `${host}:${port}`;
 
